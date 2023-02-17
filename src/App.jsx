a9d900e783aee57732e7eac33f2e90dd555cec3e
pinagui <guilhermepinaramos@gmail.com>
2023-02-17 16:52:16 -0300

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Header from './components/Header';
 import Content1 from './components/Content1';
 import {Spacer} from './assets/style';
 import Content2 from './components/Content2';
+import Hero from './components/Hero';
 
 export default function App() {
   return (
     <div>
       <Container>
-        <NavBar />
-        <Spacer />
-        <Header />
+        {/* <NavBar />
+        <Spacer /> */}
+        {/* <Header /> */}
+        <Hero />
         <Spacer space="40px" />
         <Content1 />
         <Spacer space="40px" />
